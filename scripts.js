46ebb9332c817e1c9007d2e2ca97110afb334abf
Michael <michaelk99@gmail.com>
2022-07-14 18:24:38 -0700

    Add pixelNum button to change number of rows and columns

--- scripts.js
+++ scripts.js
@@ -3,24 +3,23 @@ const container = document.createElement('div');
     container.classList.add('container');
     document.body.appendChild(container);
 
-
-
-
 //make grid for pixels
 const gridContainer = document.createElement('div');
         gridContainer.classList.add('gridContainer');
         gridContainer.style.display = 'grid';
-        gridContainer.style.width = '500px';
-        gridContainer.style.height = '500px';
+        gridContainer.style.width = '600px';
+        gridContainer.style.height = '600px';
         gridContainer.style.justifyContent = 'center';
         gridContainer.style.gap = '1px 1px';
         gridContainer.style.backgroundColor = 'red';
         gridContainer.style.gridTemplateColumns = 'repeat(16, 1fr)';
+        gridContainer.style.gridTemplateRows = 'repeat(16, 1fr)';
 
         container.appendChild(gridContainer);
 
 //make square 'pixels' to put into grid
 function pixels(...args) {
+
     for (let i = 0; i < args; ++i) {
         const pixel = document.createElement('div');
               pixel.classList.add('pixel');
@@ -32,20 +31,26 @@ function pixels(...args) {
               pixel.addEventListener('mouseenter', () => {
                 pixel.style.backgroundColor = 'blue';
               })
-              console.log(' pixel test');
     }
 }
 pixels(256);
 
-//button recieves input from user to change nuber of pixels
+//button recieves input from user to change number of pixels
 function pixelNum() {
+
     const btnDiv = document.createElement('div');
     const pixelNumBtn = document.createElement('button');
           pixelNumBtn.classList.add('pixelNumBtn');
+
+          //get input from user
           pixelNumBtn.addEventListener('click', () => {
             let input = prompt('how many pixels?');
-
+            //change grid rows and cols
+            gridContainer.style.gridTemplateColumns = 'repeat('+input+', 1fr)';
+            gridContainer.style.gridTemplateRows = 'repeat('+input+', 1fr)';
+            pixels(input * input);
           });
+
           pixelNumBtn.textContent = 'pixel number';
           btnDiv.appendChild(pixelNumBtn);
           container.insertBefore(pixelNumBtn, gridContainer)
