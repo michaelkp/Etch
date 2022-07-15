93e1d726aa9453eede6d2fd7363e68618a8d5389
Michael <michaelk99@gmail.com>
2022-07-15 11:59:20 -0700

    Add userInput to get number of cols and rows from user

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+let input = ''; //set global variable
+
 //make container
 const container = document.createElement('div');
     container.classList.add('container');
@@ -37,20 +39,47 @@ function pixels(...args) {
 }
 pixels(256);
 
+
+//get user input to change number of pixels on grid
+function userInput() {
+    input = prompt('how many pixels?');
+    //user clicks cancel
+    if (input == null) {
+        return;
+    }
+    //check if input is a number
+    while (isNaN(input)) {
+        input = prompt('Please enter a number.');
+        if (input == null) {
+            return;
+        }
+    }
+    //limit input to 100
+    while (input < 16 || input > 100){
+        input = prompt('Please enter a number from 16 - 100');
+        if (input == null) {
+            return;
+        }
+    }
+}
+
+
 //button recieves input from user to change number of pixels
 function pixelNum() {
-
+    
     const btnDiv = document.createElement('div');
     const pixelNumBtn = document.createElement('button');
           pixelNumBtn.classList.add('pixelNumBtn');
 
-          //get input from user
+         
           pixelNumBtn.addEventListener('click', () => {
-            let input = prompt('how many pixels?');
+            userInput();// get user input
+            console.log(input + ' input test')
             //change grid rows and cols
             gridContainer.style.gridTemplateColumns = 'repeat('+input+', 1fr)';
+            console.log(input + 'cols input test')
             gridContainer.style.gridTemplateRows = 'repeat('+input+', 1fr)';
-            pixels(input * input);
+            pixels(input * input);//change amount of pixels
           });
 
           pixelNumBtn.textContent = 'pixel number';
@@ -58,5 +87,6 @@ function pixelNum() {
           container.insertBefore(pixelNumBtn, gridContainer)
 }
 pixelNum();
+
 
 
