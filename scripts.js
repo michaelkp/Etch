6a983ffb5d84f865e2c8f6dd03563336a9bebc7c
Michael <michaelk99@gmail.com>
2022-07-17 03:36:54 -0700

    Reset Color button now resets both drawColor and randomColor

--- scripts.js
+++ scripts.js
@@ -4,16 +4,16 @@ const grid = document.querySelector('#grid');
 
 let pixel = document.createElement('div');
     pixel.classList.add('pixel');
-    pixel.addEventListener('mouseenter', () => {
-        pixel.classList.add('drawColor');
-    })
+    //pixel.addEventListener('mouseover', () => {
+        //pixel.classList.add('drawColor');
+    //})
     grid.appendChild(pixel);
 
 function drawPixels(...args) {
     for(let i = 0; i < args; i++) {
         let drawPix = document.createElement('div');
         drawPix.classList.add('pixel');
-        drawPix.addEventListener('mouseenter', () => {
+        drawPix.addEventListener('mouseover', () => {
             drawPix.classList.add('drawColor');
             console.log('pix test')
         })
@@ -22,13 +22,45 @@ function drawPixels(...args) {
 }
 drawPixels(256);
 
-function resetPixels() {
-    let reset = document.getElementsByClassName('drawColor');
-
-    while(reset.length) {
-        reset[0].classList.remove('drawColor');
+function randomColor() {
+    let randomColorBG = '#'+Math.floor(Math.random()*16777215).toString(16);
+    let randomPixelColor = document.getElementsByClassName('pixel');
+        
+    for(let i = 0; i < randomPixelColor.length; i++) {
+        randomPixelColor[i].addEventListener('mouseover', () => {
+            randomPixelColor[i].classList.remove('drawColor');
+            randomPixelColor[i].classList.add('randomPixelColor');
+            randomPixelColor[i].style.backgroundColor = randomColorBG;
+        })
     }
 }
+
+
+function randomColorBtn() {
+    const randomColorBtn = document.createElement('button');
+        randomColorBtn.textContent = 'Random Colors';
+        container.insertBefore(randomColorBtn, grid);
+        randomColorBtn.addEventListener('click', randomColor);
+}
+randomColorBtn();
+
+function resetPixels() {
+    const resetDraw = document.getElementsByClassName('drawColor');
+    
+    while(resetDraw.length) {
+        resetDraw[0].classList.remove('drawColor');
+        console.log(resetDraw[0] + 'resetdraw teest')
+    }
+    const resetRandomColor = document.getElementsByClassName('randomPixelColor');
+
+    while(resetRandomColor.length) {
+        resetRandomColor[0].style.backgroundColor = 'mintcream';
+        resetRandomColor[0].classList.remove('randomPixelColor');
+        
+        console.log(resetRandomColor[0] + ' remove test')
+    }
+}
+
 
 function resetGrid() {
     grid.style.gridTemplateColumns = 'repeat(16, 1fr)';
@@ -36,13 +68,17 @@ function resetGrid() {
 }
 
 function resetBtn() {
-    const resetBtn = document.createElement('button');
-    resetBtn.textContent = 'Reset Colors';
-    resetBtn.addEventListener('click', () => {
-        resetPixels();
-        resetGrid();
-    });
-    container.insertBefore(resetBtn, grid);
+    const resetColorsBtn = document.createElement('button');
+    const resetGridBtn = document.createElement('button');
+    
+    resetColorsBtn.textContent = 'Reset Colors';
+    resetGridBtn.textContent = 'Reset Grid';
+
+    resetColorsBtn.addEventListener('click', resetPixels);
+    resetGridBtn.addEventListener('click', resetGrid);
+
+    container.insertBefore(resetColorsBtn, grid);
+    container.insertBefore(resetGridBtn, grid);
 }
 resetBtn();
 
@@ -86,3 +122,4 @@ function newGrid() {
           });
 }
 newGrid()
+
